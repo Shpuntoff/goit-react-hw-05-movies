eded1cb40f4a4019b9f07a40e9a4170ed7455d21
Shpuntoff <v.shpuntoff@gmail.com>
2022-10-06 01:31:01 +0300

    upd

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Loader from 'components/Loader/Loader';
+import Navigation from 'components/Navigation/Navigation';
+
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('../pages/MovieDetailsPage/MovieDetailsPage')
+);
+const Cast = lazy(() => import('../pages/CastPage/CastPage'));
+const Reviews = lazy(() => import('../pages/ReviewsPage/ReviewsPage'));
+
+export const App = () => {
+  return (
+    <>
+      <Navigation />
+      <Suspense fallback={<Loader color={'#3f51b5'} size={32} />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
+  );
+};
